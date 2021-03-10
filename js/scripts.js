ac17519c37102824760c90a710cef1fef851393c
Matt <matt.leonard.22@gmail.com>
2021-03-10 10:31:15 -0800

    Sets up navbar links

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+
+function aboutScroll() {
+  let aboutSection = document.getElementById("aboutSection");
+  aboutSection.scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function projectScroll() {
+  let projectSection = document.getElementById("projectSection");
+  projectSection.scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function talkScroll() {
+  let talkSection = document.getElementById("talkSection");
+  talkSection.scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function contactScroll() {
+  let contactSection = document.getElementById("contactSection");
+  contactSection.scrollIntoView({
+    behavior: "smooth"
+  });
+}
