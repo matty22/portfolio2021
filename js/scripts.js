a78f0cec5b9b4b3db014f029d0100e1d87afcc80
Matt <matt.leonard.22@gmail.com>
2021-03-11 18:49:37 -0800

    adds hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -93,5 +93,14 @@ function contactScroll() {
 }
 
 
+// Burger menu scripts
+
+function toggleBurger() {
+  let burgerIcon = document.getElementById("navbarBurger");
+  let navMenu = document.getElementById("navMenu");
+  burgerIcon.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+}
+
 
 
